93c4879f9db993b592a878176a8dbfe573634c0b
steven.ke <steven.ke@nike.com>
2023-01-10 15:35:35 +0800

    ONENCP-127 disable EnableWebMvc

--- src/main/java/com/nike/ncp/common/configuration/SwaggerConfiguration.java
+++ src/main/java/com/nike/ncp/common/configuration/SwaggerConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
