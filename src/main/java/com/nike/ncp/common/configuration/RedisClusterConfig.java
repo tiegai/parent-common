064de7df46580d4fbd19100dcd1e188159373629
york <york.ye@nike.com>
2023-01-16 11:53:21 +0800

    Integrated redis service

---
+++ src/main/java/com/nike/ncp/common/configuration/RedisClusterConfig.java
@@ -0,0 +1,53 @@
+package com.nike.ncp.common.configuration;
+
+import com.nike.ncp.common.properties.RedisClusterProperties;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Getter
+@Configuration
+@ConditionalOnProperty(value = "spring.redis.enabled", havingValue = "true")
+public class RedisClusterConfig {
+
+    @Autowired
+    private RedisClusterProperties clusterProperties;
+
+    @Bean
+    public RedisConnectionFactory connectionFactory() {
+        return new LettuceConnectionFactory(
+                new RedisClusterConfiguration(clusterProperties.getNodes()));
+    }
+
+    @Bean
+    public RedisTemplate<String, Long> redisTemplate() {
+        RedisTemplate<String, Long> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        redisTemplate.setConnectionFactory(connectionFactory());
+        return redisTemplate;
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate() {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
+        stringRedisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        stringRedisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        stringRedisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        stringRedisTemplate.setConnectionFactory(connectionFactory());
+        return stringRedisTemplate;
+    }
+
+}
