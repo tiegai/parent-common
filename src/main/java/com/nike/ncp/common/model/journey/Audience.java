accc5019920071db4b7c1910e57d8e4c983f5b6c
Jon HE <jon.he@nike.com>
2023-01-30 16:33:58 +0800

    update model

--- src/main/java/com/nike/ncp/common/model/journey/Audience.java
+++ src/main/java/com/nike/ncp/common/model/journey/Audience.java
@@ -1,22 +1,32 @@
-package com.nike.ncp.common.model.document;
+package com.nike.ncp.common.model.journey;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 import java.util.List;
 
 @Data
 public class Audience {
+    @Id
+    private String id;
     private String nextActivityId;
     private String upmid;
     private String nuid;
     private String phone;
     private String email;
     private List<Trait> traits;
+    private List<FreqCtrlRes> freqCtrlRes;
 
     @Data
     public static class Trait {
         private String traitId;
         private String traitName;
+        private String value;
+    }
+
+    @Data
+    public static class FreqCtrlRes {
+        private String tag;
         private String value;
     }
 }
