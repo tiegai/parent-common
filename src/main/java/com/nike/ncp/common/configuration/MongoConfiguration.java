71e9b91862d6a935cfb86b5cddce39c55ffc2c12
york <york.ye@nike.com>
2023-02-06 15:05:56 +0800

    add config of remove the _class field inserted into the database

--- src/main/java/com/nike/ncp/common/configuration/MongoConfiguration.java
+++ src/main/java/com/nike/ncp/common/configuration/MongoConfiguration.java
@@ -1,21 +1,44 @@
 package com.nike.ncp.common.configuration;
 
 import com.mongodb.MongoClientSettings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.boot.autoconfigure.mongo.MongoPropertiesClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @Configuration
 public class MongoConfiguration {
+
+    @Autowired
+    private MongoDatabaseFactory mongoDatabaseFactory;
+
+    @Autowired
+    private MongoMappingContext mongoMappingContext;
 
     private final MongoProperties mongoProperties;
 
     public MongoConfiguration(MongoProperties mongoProperties) {
         super();
         this.mongoProperties = mongoProperties;
+    }
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter() {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        // Here is to remove the _class field inserted into the database.
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        return converter;
     }
 
     @Bean
