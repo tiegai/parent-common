022f3a49a926e840fb30a6d6d418a5187e053618
Yizhi Qian <yizhi.qian@nike.com>
2023-02-08 12:04:27 +0800

    ONENCP-222 ObjectId custom serialization

---
+++ model/src/main/java/com/nike/ncp/common/model/util/ObjectIdSerializer.java
@@ -0,0 +1,15 @@
+package com.nike.ncp.common.model.util;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.bson.types.ObjectId;
+
+import java.io.IOException;
+
+public class ObjectIdSerializer extends JsonSerializer<ObjectId> {
+    @Override
+    public void serialize(ObjectId objectId, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeString(objectId.toString());
+    }
+}
