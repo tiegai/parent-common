ef4cf431b91d8ac5d9d1d5900c450b2ec85cee13
steven.ke <steven.ke@nike.com>
2023-02-10 14:51:05 +0800

    print springboot exception for debugging

--- src/main/java/com/nike/ncp/common/controller/ApiExceptionHandler.java
+++ src/main/java/com/nike/ncp/common/controller/ApiExceptionHandler.java
@@ -6,11 +6,14 @@ import com.nike.ncp.common.exception.ApiExceptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.util.WebUtils;
 
 @Slf4j
 @ControllerAdvice
@@ -37,5 +40,15 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         ApiException exception = ApiExceptions.internalError();
         ApiExceptionResponse response = ApiExceptionResponse.builder().appName(appName).errors(exception.getErrors()).build();
         return handleExceptionInternal(ex, response, new HttpHeaders(), exception.getStatus(), request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        log.error("internal_exception, ex={}", ex.getMessage(), ex);
+        if (HttpStatus.INTERNAL_SERVER_ERROR.equals(status)) {
+            request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, WebRequest.SCOPE_REQUEST);
+        }
+        return new ResponseEntity<>(body, headers, status);
     }
 }
